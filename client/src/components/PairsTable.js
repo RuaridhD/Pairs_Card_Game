dc37ca0aa2922c7898a73c7f0b3411fbf5ce52d9
Daniel Childs <dc.1994@outlook.com>
2018-06-29 13:41:43 +0100

    add tile class

--- client/src/components/PairsTable.js
+++ client/src/components/PairsTable.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
+import Tile from "./Tile.js"
 
 class PairsTable extends Component {
   render() {
     return(
       <div>
-        {/* <p></p> */}
+
       <p>Pairs table</p>
+      <Tile />
       </div>
     )
   } // end of render
