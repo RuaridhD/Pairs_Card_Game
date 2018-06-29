2ed038c09ef31d95a4cbeffbc3d55bd37276a97b
Daniel Childs <dc.1994@outlook.com>
2018-06-29 13:35:05 +0100

    add pair container class

---
+++ client/src/containers/PairContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class PairContainer extends Component {
+  render() {
+    return(
+      <p>Contianer</p>
+    )
+  }
+
+}
+
+
+
+export default PairContainer;
