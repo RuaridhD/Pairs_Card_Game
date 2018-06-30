73c6642cabef37e35f6ef7a724007982fc382d86
Daniel Childs <dc.1994@outlook.com>
2018-06-30 17:22:13 +0100

    populates navbar and imports react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PairContainer from "./containers/PairContainer"
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
 class App extends Component {
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+  <ul>
+    <li>
+      {/* <Link to="/">Home </Link> */} Home
+    </li>
+    <li>
+      {/* <Link to="/pairs">Pairs </Link> */} Pairs
+    </li>
+    <li>
+      {/* <Link to="/about">About </Link> */} About
+    </li>
+  </ul>
+}
+
+
+export default NavBar
