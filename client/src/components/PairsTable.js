3a6578a33ff6e29267a06292cc20939c1876945f
omikron15 <omikron15@hotmail.co.uk>
2018-06-30 11:02:39 +0100

    adds method to check for pairs and removes found pairs from board

--- client/src/components/PairsTable.js
+++ client/src/components/PairsTable.js
@@ -5,11 +5,16 @@ class PairsTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      card1Value: null,
+      card1Index: null,
+      card2Value: null,
+      card2Index: null
     }
     this.handleTileClick = this.handleTileClick.bind(this);
     this.enableTiles= this.enableTiles.bind(this);
     this.disableTiles = this.disableTiles.bind(this);
+    this.checkPairs = this.checkPairs.bind(this);
 
   }
   render() {
@@ -32,15 +37,43 @@ handleTileClick(event) {
 
   this.setState({counter: tempCounter += 1});
 
+  if(tempCounter ===1 ){
+    this.setState({card1Value: this.props.deck[event.target.value].value})
+    this.setState({card1Index: event.target.id})
+  }
+
+
   event.target.innerText = this.props.deck[event.target.value].value;
 
   event.target.disabled = true;
   if (tempCounter === 2){
+    this.setState({card2Value: this.props.deck[event.target.value].value})
+    this.setState({card2Index: event.target.id})
     this.disableTiles();
-    setTimeout(this.enableTiles, 2000);
+    setTimeout(this.checkPairs, 2000);
     this.setState({counter: 0});
   }
 }
+
+checkPairs(){
+
+  console.log("in check pairs");
+  const button1 = document.getElementById(this.state.card1Index)
+  const button2 = document.getElementById(this.state.card2Index)
+
+  if(this.state.card1Value === this.state.card2Value){
+    button1.hidden = true;
+    button2.hidden = true;
+  }
+
+  button1.innerText = ""
+  button2.innerText = ""
+
+  this.enableTiles()
+
+}
+
+
 
 disableTiles(){
   const buttons = document.querySelectorAll('.tile-button')
--- client/src/components/Tile.js
+++ client/src/components/Tile.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Tile extends Component {
   render() {
     return(
-      <button className="tile-button" value={this.props.index} onClick={this.props.onClickMethod}></button>
+      <button id = {this.props.index} className="tile-button" value={this.props.index} onClick={this.props.onClickMethod}></button>
     )
   }
 
