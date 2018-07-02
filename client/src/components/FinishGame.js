30d4520a680ad67a1abb0c7e8b226ffcd32d6a8b
Daniel Childs <dc.1994@outlook.com>
2018-07-02 14:39:31 +0100

    adds table to finishgame (incomplete)

--- client/src/components/FinishGame.js
+++ client/src/components/FinishGame.js
@@ -2,9 +2,38 @@ import React,{Component} from 'react'
 
 class FinishGame extends Component {
   render () {
+    // here we want to loop through all of the players to see who has the most pairs. Then populate
+    // a table with the player with the most pairs at the top.
+
+    var gameStats = []
+    for (var player in this.props.pairs){
+      console.log(this.props.pairs);
+      gameStats.push([player, this.props.pairs[player]])
+    }
+      gameStats.sort(function(a, b) {
+        return b[1] - a[1];
+      })
+
+      var tableData = gameStats.map((player, index) => {
+        console.log(gameStats);
+
+        {player[0]}
+
+      
+      })
 
     return (
-        <p>Render Test</p>
+      <div>
+        <table>
+          <tr>
+            <th>Player</th>
+            <th>Score</th>
+          </tr>
+
+
+        </table>
+  {tableData}
+        </div>
 
     )
   }// end of render
