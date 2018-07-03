7d5e9c34723d81e7fbb5d1d053fbb18c71747a5c
omikron15 <omikron15@hotmail.co.uk>
2018-07-03 21:50:10 +0100

    Refactors playerSelector component to correct tbody and key errors

--- client/src/games/pairs/components/PlayersSelector.js
+++ client/src/games/pairs/components/PlayersSelector.js
@@ -56,22 +56,22 @@ class PlayersSelector extends Component {
   render() {
     var renderPlayers = null;
     if (this.state.selectorFlag) {
-      const playersList = this.state.players.map(player => {
+      const playersList = this.state.players.map((player, index) => {
         if(this.state.activePlayers.includes(player)){
-          return <option id={player} value={player} disabled>{player}</option>
+          return <option key={index} id={player} value={player} disabled>{player}</option>
         } else {
-          return <option id={player} value={player}>{player}</option>
+          return <option key={index} id={player} value={player}>{player}</option>
         }
       })
 
-      const tableList = this.state.activePlayers.map(player => (
-        <tr>
-          <td>{player}</td>
+      const tableList = this.state.activePlayers.map((player, index) => (
+        <tr key={index}>
+          <td >{player}</td>
         </tr>
       ))
 
       renderPlayers =
-      <div class="players-selector-div">
+      <div className="players-selector-div">
         <div>
           <select id="player-dropdown">
             <option hidden disabled selected value="$default$">Select a player...</option>
@@ -80,10 +80,13 @@ class PlayersSelector extends Component {
         </div>
 
         <table>
+          <tbody>
           <tr>
             <th>Players</th>
           </tr>
+
           {tableList}
+          </tbody>
         </table>
         <button onClick={this.addPlayerToTable}>Add Player</button>
         <button onClick={this.startButtonClicked}>Start Game</button>
@@ -91,13 +94,13 @@ class PlayersSelector extends Component {
       </div>
     } else {
       renderPlayers =
-      <div class="players-selector-div">
+      <div className="players-selector-div">
         <h2>How many players?</h2>
-        <div class="players-selector-btns-div">
-          <button id="1-player" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="1">1 Player</button>
-          <button id="2-players" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="2">2 Players</button>
-          <button id="3-players" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="3">3 Players</button>
-          <button id="4-players" class="players-selector-btn" onClick= {this.selectNumberOfPlayers} value="4">4 Players</button>
+        <div className="players-selector-btns-div">
+          <button id="1-player" className="players-selector-btn" onClick={this.selectNumberOfPlayers} value="1">1 Player</button>
+          <button id="2-players" className="players-selector-btn" onClick={this.selectNumberOfPlayers} value="2">2 Players</button>
+          <button id="3-players" className="players-selector-btn" onClick={this.selectNumberOfPlayers} value="3">3 Players</button>
+          <button id="4-players" className="players-selector-btn" onClick= {this.selectNumberOfPlayers} value="4">4 Players</button>
         </div>
       </div>
     }
