bf1b5cc044cf7f69ce297b7ddf6b3ee580160bdb
Brian Cooke <brian.cooke@live.co.uk>
2018-07-03 12:18:56 +0100

    Adds logic for rendering in  PlayerSelector component

--- client/src/components/PlayersSelector.js
+++ client/src/components/PlayersSelector.js
@@ -6,7 +6,8 @@ class PlayersSelector extends Component {
     super(props)
     this.state = {
       numberOfPlayers: null,
-      selectorFlag: false
+      selectorFlag: false,
+      players: []
     }
     this.selectNumberOfPlayers = this.selectNumberOfPlayers.bind(this)
   }
@@ -15,19 +16,42 @@ class PlayersSelector extends Component {
     var renderPlayers = null;
 
     if (this.state.selectorFlag) {
-      renderPlayers = <h1>It works!</h1>
+      const playersList = this.state.players.map(player => (
+        <option value="">{player}</option>
+      ))
+
+      renderPlayers =
+      <div>
+        <select>
+          <option hidden selected>Select a player...</option>
+          {playersList}
+        </select>
+
+        <table>
+          <tr>
+            <th>Players</th>
+          </tr>
+
+          {/* {this.tablePlayers} */}
+
+        </table>
+        <button onClick="">Add Player</button>
+        <button onClick="">Start Game</button>
+
+
+      </div>
     } else
     {
       renderPlayers =
-        <div id="players-selector-div">
-          <h2>How many players?</h2>
-          <div id="players-selector-btns-div">
-            <button id="1-player" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="1">1 Player</button>
-            <button id="2-players" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="2">2 Players</button>
-            <button id="3-players" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="3">3 Players</button>
-            <button id="4-players" class="players-selector-btn" onClick= {this.selectNumberOfPlayers} value="4">4 Players</button>
-          </div>
+      <div id="players-selector-div">
+        <h2>How many players?</h2>
+        <div id="players-selector-btns-div">
+          <button id="1-player" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="1">1 Player</button>
+          <button id="2-players" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="2">2 Players</button>
+          <button id="3-players" class="players-selector-btn" onClick={this.selectNumberOfPlayers} value="3">3 Players</button>
+          <button id="4-players" class="players-selector-btn" onClick= {this.selectNumberOfPlayers} value="4">4 Players</button>
         </div>
+      </div>
     }
 
 
@@ -40,12 +64,28 @@ class PlayersSelector extends Component {
 
   }
 
+  componentDidMount() {
+    this.populatePlayers()
+  }
+
   selectNumberOfPlayers(event) {
     this.setState({
       numberOfPlayers: event.target.value,
       selectorFlag: true
     })
+  }
 
+  populatePlayers() {
+    const playersArray = [];
+    const url = "/api/users";
+    fetch(url)
+    .then(res => res.json())
+    .then(players => players.forEach(player => {
+      playersArray.push(player.Player);
+    })).then(playersList => this.setState({players: playersArray}))
+  }
+
+  tablePlayers() {
 
   }
 
