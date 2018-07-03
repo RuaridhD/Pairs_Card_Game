17b8d0b7e05c38a51933815fa685563bcd218d4c
Ruaridh <ruaridhdunbar@gmail.com>
2018-07-03 14:02:46 +0100

    adds get method to PlayerContainer to populate PlayerForm with seeded usernames

--- client/src/components/PlayerForm.js
+++ client/src/components/PlayerForm.js
@@ -1,19 +1,35 @@
 import React,{Component} from 'react'
 
-class Players extends Component {
+class PlayerForm extends Component {
+
+
 
   render(){
+        console.log(this.props.players);
+
+    let existingPlayers = this.props.players.map(player => (
+      <li key = {player._id}>
+        {player.Player}
+      </li>
+    ))
 
     return (
       <div>
-        <p>Players page</p>
-        <form className="create-form">
-          <input type="text" placeholder="Enter User Name"/>
-          <input type="submit" value="Create"/>
-        </form>
+        <div>
+          <p>Players page</p>
+          <form className="create-form">
+            <input type="text" placeholder="Enter User Name" value={this.props.player} onChange={this.handleTextChange}/>
+            <input type="submit" value="Create" onSubmit={this.handleSubmit}/>
+          </form>
+        </div>
+        <div>
+          <ul>
+            <li>{existingPlayers}</li>
+          </ul>
+        </div>
       </div>
     ) // end of return
   } // end of players
 }
 
-export default Players;
+export default PlayerForm;
--- client/src/containers/PlayerContainer.js
+++ client/src/containers/PlayerContainer.js
@@ -6,9 +6,47 @@ class PlayerContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      player: '',
       players: []
     }
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-}
 
-export default PlayerContainer;
+
+  handleTextChange(event){
+    this.setState({text: event.target.value})
+  }
+
+  componentDidMount(){
+    const url = '/api/users'
+    fetch(url)
+    .then(res => res.json())
+    .then(players =>
+      this.setState({
+        players: players
+      }));
+    }
+
+    handleSubmit(event){
+      event.preventDefault()
+      var player = this.state.player.trim()
+      if (!player){
+        return
+      }
+      this.setState({player: player})
+
+
+    }
+
+
+
+
+
+    render(){
+      return (<PlayerForm players={this.state.players}/>)
+    }
+
+  }
+
+  export default PlayerContainer;
