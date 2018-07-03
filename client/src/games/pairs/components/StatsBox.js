762d7cffa5572fc7b403506ade59af6130131f43
omikron15 <omikron15@hotmail.co.uk>
2018-07-03 21:56:23 +0100

    Refactors StatsBox component to correct tbody and key errors

--- client/src/games/pairs/components/StatsBox.js
+++ client/src/games/pairs/components/StatsBox.js
@@ -5,7 +5,7 @@ class StatsBox extends Component {
 
   render() {
     var playersList = this.props.players.map((player,index) =>(
-      <tr>
+      <tr key={index}>
         <td>{player}</td>
         <td>{this.props.turns[player]}</td>
         <td>{this.props.pairs[player]}</td>
@@ -15,12 +15,14 @@ class StatsBox extends Component {
       <div id="stats-box">
         <h3 id="stats-heading">Game Stats</h3>
         <table>
+          <tbody>
           <tr>
-            <th class="stats-table-heading">Player</th>
-            <th class="stats-table-heading">Turns</th>
-            <th class="stats-table-heading">Pairs</th>
+            <th className="stats-table-heading">Player</th>
+            <th className="stats-table-heading">Turns</th>
+            <th className="stats-table-heading">Pairs</th>
           </tr>
           {playersList}
+          </tbody>
         </table>
       </div>
     )
