687c871a658931322127d35b8e144294c3b4de55
Daniel Childs <dc.1994@outlook.com>
2018-07-03 11:18:33 +0100

    adds create-form

--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -3,9 +3,13 @@ import React,{Component} from 'react'
 const Players = () => {
 
   return (
+    <div>
     <p>Players page</p>
-    <p>Players page</p>
-
+    <form className="create-form">
+      <input type="text" placeholder="Enter User Name"/>
+      <input type="submit" value="Create"/>
+    </form>
+  </div>
   ) // end of retrun
 } // end of players
 
